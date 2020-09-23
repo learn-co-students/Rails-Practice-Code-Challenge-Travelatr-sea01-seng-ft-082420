b5560cc8dac2bf93ac7c497230fb50e9a2a9af32
Chanson9892 <chan.hanson5@gmail.com>
2020-09-22 20:49:10 -0700

    Done.

--- app/models/destination.rb
+++ app/models/destination.rb
@@ -1,3 +1,6 @@
 class Destination < ApplicationRecord
-
+    has_many :posts
+    has_many :bloggers, through: :posts
+    validates :name, presence: true
+    validates :country, presence: true
 end
